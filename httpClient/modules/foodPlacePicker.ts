bfa3f6357cb018017fcc0a7a64fe6f984985a940
Stanico Knowles <65465649+Stanico-Knowles@users.noreply.github.com>
2022-11-10 16:37:46 -0500

    add enum for toast types for re-usability

--- httpClient/modules/foodPlacePicker.ts
+++ httpClient/modules/foodPlacePicker.ts
@@ -1,16 +1,17 @@
 import { IFoodPlaceCrtieria } from "../../types/IFoodPlaceCriteria";
 import sendToast from "../../utils/toast";
 import httpRequest from "../api";
+import { ToastTypes } from "../../enums/ToastTypes";
 
 const actions = {
     async getFoodPlace(data: IFoodPlaceCrtieria) {
         try {
             const endpoint = `/food-place?rating=${data.rating}&price=${data.price}&distance=${data.distance}`;
             const resp = await httpRequest.get(endpoint)
-            return sendToast(resp.data['foodPlace'], 'success')
+            return sendToast(resp.data['foodPlace'], ToastTypes.SUCCESS)
         }
         catch(err: any) {
-            return sendToast(err.response.data.error, 'error')
+            return sendToast(err.response.data.error, ToastTypes.ERROR)
         } 
     },
 
